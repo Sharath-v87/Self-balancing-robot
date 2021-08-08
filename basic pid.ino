3216d412a9fe8b499359defac5d32dc35e5ac732
Shawrat <sharathvenkateswaran@gmail.com>
2021-08-08 22:25:48 +0530

    half ass'd code

--- basic pid.ino
+++ basic pid.ino
@@ -9,10 +9,11 @@ int leftpwm = 5;
 int rightpwm = 7;
 int left = 6;
 int right = 8;
-
+float maximum=0;
+float minimum=1023;
 int kp = 2;
-int kd = 5;
-int ki = 6;
+int kd = 0;
+int ki = 0;
 float setpt;
 float ip, op;
 int currentime, elapsedtime, prevtime;
@@ -22,11 +23,16 @@ float gyroang = 0;
 float currentang = 0;
 float prevang = 0;
 float motorip = 0;
+float gyrorate;
+int looptime, curtime, pretime;
+float sampleTime = 0.005;
+
 
 void wheels (int leftwheel, int rightwheel){
     if (leftwheel >= 0){
         analogWrite(leftpwm, leftwheel);
         digitalWrite(left, LOW);
+    }
     else{
         analogWrite(leftpwm, 255+leftwheel);
         digitalWrite(left, LOW);
@@ -34,59 +40,84 @@ void wheels (int leftwheel, int rightwheel){
     if (rightwheel >= 0){
         analogWrite(rightpwm, rightwheel);
         digitalWrite(right, LOW);
+    } 
     else{
         analogWrite(rightpwm, 255+rightwheel);
         digitalWrite(right, LOW);
     }
-    }
+    
+}
+  
+void init_PID() {  
+  // initialize Timer1
+  cli();          // disable global interrupts
+  TCCR1A = 0;     // set entire TCCR1A register to 0
+  TCCR1B = 0;     // same for TCCR1B    
+  // set compare match register to set sample time 5ms
+  OCR1A = 9999;    
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS11 bit for prescaling by 8
+  TCCR1B |= (1 << CS11);
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  sei();          // enable global interrupts
 }
 
-float pid(float ip){
-    currentime = millis();
-    elapsedtime = currentime - prevtime;
-        
-    error = setpt - ip;
-    integerror += error*elapsedtime;
-    differror = (error - preverror)/elapsedtime;
-
-    k = (kp*error) + (kd*differror) + (ki*integerror);
-
-    prevtime = currentime;
-    preverror = error;
-
-    return k;
-}
 
 void setup(){
     Serial.begin(9600);
     Wire.begin();
     mpu6050.begin();
-    mpu6050.setYAccelOffset(1593);
-    mpu6050.setZAccelOffset(963);
-    mpu6050.setXGyroOffset(40);
+    mpu6050.calcGyroOffsets(true);
+    pinMode(leftpwm, OUTPUT);
+    pinMode(left, OUTPUT);
+    pinMode(rightpwm, OUTPUT);
+    pinMode(right, OUTPUT);
     setpt=0;
+    init_PID();
 }
 
 void loop(){
-    curtime = millis();
-    looptime = curtime - pretime;
-    pretime = curtime;
     
     mpu6050.update();
     Accy = mpu6050.getAccY();
     Accz = mpu6050.getAccZ();
     gyrox = mpu6050.getGyroX();
 
-    accangle = atan2(Accy,Accz)*RAD_TO_DEG;
+    //Serial.println(gyrox);
 
-    gyrorate = map(gyrox,-32768, 32767, -250, 250);
-    gyroang = gyroang + gyrorate * (looptime/1000);
-
-    currentang = 0.9934*(prevangle+gyroang)+0.0066*accangle;
     
-    motorip = pid(currentang);
     motorip = constrain(motorip,-255,255);
     wheels(motorip,motorip);
-    prevang = currentang;
 
+    
+
+}
+
+ISR(TIMER1_COMPA_vect){
+  accangle = atan2(Accy,Accz)*RAD_TO_DEG;
+
+    
+    
+  gyrorate = map(gyrox,-32768, 32767, -250, 250);
+  gyroang = (float)gyrorate*sampleTime;
+
+  currentang = 0.9934*(prevang+gyroang)+0.0066*accangle;
+    
+    //Serial.println(gyrox);
+    
+        
+  error = currentang - setpt;
+  integerror += error*sampleTime;
+  differror = (error - preverror)/sampleTime;
+
+  k = (kp*error) + (kd*differror) + (ki*integerror);
+
+  prevtime = currentime;
+  preverror = error;
+
+
+  motorip = k;
+  prevang = currentang;
 }
